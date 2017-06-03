45e3f22810c61cacdfdedb0a4d19c220b12a3350
Jason-WOW <2568325015@qq.com>
2017-06-03 16:23:11 +0800

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private readonly string Token = "weixin";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Auth();
+    }
+    private void Auth()
+    {
+        string signature = Request["signature"];
+        string timestamp = Request["timestamp"];
+        string nonce = Request["nonce"];
+        string echoste = Request["echostr"];
+        if (Request.HttpMethod == "GET")
+        {
+            if (CheckSignature.Check(signature, timestamp, nonce, Token))
+            {
+                Response.Write(echoste);
+            }
+            else
+            {
+                Response.Write("kyi");
+            }
+        }
+    }
+}
