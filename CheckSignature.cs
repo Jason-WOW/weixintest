5c257ecc4da9db71330ffc77fb0d21f163f50f10
Jason-WOW <2568325015@qq.com>
2017-06-03 16:23:37 +0800

    Add files via upload

---
+++ CheckSignature.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.Security;
+
+/// <summary>
+/// CheckSignature 的摘要说明
+/// </summary>
+public class CheckSignature
+{
+    public const string Token = "weixin";
+    public CheckSignature()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+         
+    }
+    public static bool Check(string signature, string timestamp, string nonce, string token)
+    {
+        return signature == GetSignature(timestamp,nonce,token);
+    }
+    public static string GetSignature(string timestamp, string noce, string token = Token)
+    {
+        token = token ?? Token;
+        string[] arr = new[] { token,timestamp,noce}.OrderBy(z=>z).ToArray();
+        string arrstring = string.Join("",arr);
+        var tempsign = FormsAuthentication.HashPasswordForStoringInConfigFile(arrstring, "SHA1").ToLower();
+        return tempsign;
+    }
+}
